e8aa024ec60c146bbd59007a569a9db99575a447
tomohiro.kojima <niniseseko@gmail.com>
2021-08-25 22:27:08 +0000

    Q13,Q15再提出

--- task.rb
+++ task.rb
@@ -118,8 +118,7 @@ def q13
   update_data = { age: 32, address: "沖縄" }
 
   # 以下に回答を記載
-  user_data[:age] = update_data[:age]
-  user_data[:address] = update_data[:address]
+  user_data.update(update_data)
   p user_data
 end
 
@@ -135,17 +134,8 @@ def q15
   data2 = { name: "yamada", hobby: "baseball", role: "normal" }
 
   # 以下に回答を記載
-  if data1.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
-  
-  if data2.key?(:age)
-    puts "OK"
-  else
-    puts "NG"
-  end
+  puts data1.key?(:age) ? "OK" : "NG"
+  puts data2.key?(:age) ? "OK" : "NG"
 end
 
 def q16
